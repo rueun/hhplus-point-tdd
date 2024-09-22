92b46b6fd56776f3362234aacf6f35555c7334c7
rueun <dkfah1229@naver.com>
2024-09-22 22:16:18 +0900

    test: userPoint 테스트 코드 변경

--- src/test/java/io/hhplus/tdd/point/domain/model/entity/UserPointTest.java
+++ src/test/java/io/hhplus/tdd/point/domain/model/entity/UserPointTest.java
@@ -3,6 +3,7 @@ package io.hhplus.tdd.point.domain.model.entity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.System.currentTimeMillis;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserPointTest {
@@ -11,18 +12,18 @@ class UserPointTest {
     @DisplayName("사용자의 포인트를 충전할 수 있다.")
     void charge_success() {
         // given
-        UserPoint userPoint = UserPoint.empty(1);
+        UserPoint userPoint = new UserPoint(1L, 100L, currentTimeMillis());
         // when
-        UserPoint chargedUserPoint = userPoint.charge(100);
+        UserPoint chargedUserPoint = userPoint.charge(100L);
         // then
-        assertEquals(100, chargedUserPoint.point());
+        assertEquals(200L, chargedUserPoint.point());
     }
 
     @Test
     @DisplayName("0 미만의 포인트는 충전할 수 없다.")
     void charge_fail_negative_point() {
         // given
-        UserPoint userPoint = UserPoint.empty(1);
+        UserPoint userPoint = new UserPoint(1L, 100L, currentTimeMillis());
 
         // when & then
         final IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> userPoint.charge(-100));
