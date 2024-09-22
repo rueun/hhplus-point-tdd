aaf2c1999f5a8797e9111cefdf3dd6b9f22ec04c
rueun <dkfah1229@naver.com>
2024-09-23 00:55:57 +0900

    test: UserPointTest 테스트 코드 변경 - 경계 값 테스트

--- src/test/java/io/hhplus/tdd/point/domain/model/entity/UserPointTest.java
+++ src/test/java/io/hhplus/tdd/point/domain/model/entity/UserPointTest.java
@@ -2,6 +2,8 @@ package io.hhplus.tdd.point.domain.model.entity;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static java.lang.System.currentTimeMillis;
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,14 +21,16 @@ class UserPointTest {
         assertEquals(200L, chargedUserPoint.point());
     }
 
-    @Test
+
+    @ParameterizedTest
+    @ValueSource(longs = {-100L, 0L})
     @DisplayName("0 미만의 포인트는 충전할 수 없다.")
-    void charge_fail_negative_point() {
+    void charge_fail_negative_point(long amount) {
         // given
         UserPoint userPoint = new UserPoint(1L, 100L, currentTimeMillis());
 
         // when & then
-        final IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> userPoint.charge(-100));
-        assertEquals("충전할 포인트는 0 이상이어야 합니다.", exception.getMessage());
+        final IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> userPoint.charge(amount));
+        assertEquals("충전할 포인트는 0보다 커야 합니다.", exception.getMessage());
     }
 }
