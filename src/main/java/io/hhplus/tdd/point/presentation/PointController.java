376d9315d8f3c3f1b79098b7a7f0c0f3792630b6
rueun <dkfah1229@naver.com>
2024-09-23 01:01:53 +0900

    feat: pointController - 유저 포인트 충전 api 구현

--- src/main/java/io/hhplus/tdd/point/presentation/PointController.java
+++ src/main/java/io/hhplus/tdd/point/presentation/PointController.java
@@ -1,18 +1,23 @@
 package io.hhplus.tdd.point.presentation;
 
+import io.hhplus.tdd.point.application.dto.ChargeUserPointCommand;
+import io.hhplus.tdd.point.application.service.PointService;
 import io.hhplus.tdd.point.domain.model.entity.PointHistory;
 import io.hhplus.tdd.point.domain.model.entity.UserPoint;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/point")
 public class PointController {
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
+    private final PointService pointService;
 
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
@@ -34,15 +39,13 @@ public class PointController {
         return List.of();
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
-     */
+
     @PatchMapping("{id}/charge")
     public UserPoint charge(
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        return pointService.charge(ChargeUserPointCommand.of(id, amount));
     }
 
     /**
