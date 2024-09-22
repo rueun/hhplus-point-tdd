e33c78136b8a6917cc2d87676b208dff711475d8
rueun <dkfah1229@naver.com>
2024-09-23 01:00:45 +0900

    fix: command 에서 amount 값 유효성 검증하지 않도록 변경

--- src/main/java/io/hhplus/tdd/point/application/dto/ChargeUserPointCommand.java
+++ src/main/java/io/hhplus/tdd/point/application/dto/ChargeUserPointCommand.java
@@ -1,19 +1,14 @@
 package io.hhplus.tdd.point.application.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ChargeUserPointCommand {
     private final long userId;
     private final long amount;
-
-    private ChargeUserPointCommand(final long userId, final long amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException("amount should be positive");
-        }
-        this.userId = userId;
-        this.amount = amount;
-    }
 
     public static ChargeUserPointCommand of(final long userId, final long amount) {
         return new ChargeUserPointCommand(userId, amount);
