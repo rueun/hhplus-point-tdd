ecd79b8419961bc1b03cfac54d14a759d357bbd8
rueun <dkfah1229@naver.com>
2024-09-24 01:08:17 +0900

    test: PointHistoryRepositoryImpl 실제 db 사용하도록 변경

--- src/test/java/io/hhplus/tdd/point/infrastructure/persistence/PointHistoryRepositoryImplTest.java
+++ src/test/java/io/hhplus/tdd/point/infrastructure/persistence/PointHistoryRepositoryImplTest.java
@@ -1,36 +1,32 @@
 package io.hhplus.tdd.point.infrastructure.persistence;
 
-import io.hhplus.tdd.point.infrastructure.persistence.database.PointHistoryTable;
 import io.hhplus.tdd.point.domain.model.entity.PointHistory;
-import io.hhplus.tdd.point.domain.model.vo.TransactionType;
+import io.hhplus.tdd.point.infrastructure.persistence.database.PointHistoryTable;
 import io.hhplus.tdd.point.infrastructure.persistence.repository.PointHistoryRepositoryImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 import static io.hhplus.tdd.point.domain.model.vo.TransactionType.CHARGE;
+import static org.assertj.core.api.Assertions.assertThat;
 import static java.lang.System.currentTimeMillis;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
 
+@SpringBootTest
 class PointHistoryRepositoryImplTest {
 
-    @Mock
+    @Autowired
     private PointHistoryTable pointHistoryTable;
 
-    @InjectMocks
     private PointHistoryRepositoryImpl pointHistoryRepository;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
+        pointHistoryRepository = new PointHistoryRepositoryImpl(pointHistoryTable);
     }
 
     @Test
@@ -38,16 +34,18 @@ class PointHistoryRepositoryImplTest {
     void insert() {
         // given
         long currentTime = currentTimeMillis();
-        final PointHistory pointHistory = new PointHistory(1L, 1L, 100L, CHARGE, currentTime);
-        given(pointHistoryTable.insert(anyLong(), anyLong(), any(TransactionType.class), anyLong()))
-                .willReturn(pointHistory);
 
         // when
         PointHistory savedPointHistory = pointHistoryRepository.insert(1L, 100L, CHARGE, currentTime);
 
         // then
-        assertNotNull(savedPointHistory);
-        assertEquals(pointHistory, savedPointHistory);
+        assertAll(
+                () -> assertNotNull(savedPointHistory),
+                () -> assertEquals(1L, savedPointHistory.userId()),
+                () -> assertEquals(100L, savedPointHistory.amount()),
+                () -> assertEquals(CHARGE, savedPointHistory.type()),
+                () -> assertEquals(currentTime, savedPointHistory.updateMillis())
+        );
     }
 
 
@@ -55,16 +53,24 @@ class PointHistoryRepositoryImplTest {
     @DisplayName("사용자 ID로 포인트 히스토리를 조회할 수 있다.")
     void selectAllByUserId() {
         // given
-        final PointHistory pointHistory = new PointHistory(1L, 1L, 100L, CHARGE, currentTimeMillis());
-        final List<PointHistory> pointHistories = List.of(pointHistory);
-        given(pointHistoryTable.selectAllByUserId(anyLong()))
-                .willReturn(pointHistories);
+        long currentTime = currentTimeMillis();
+        pointHistoryRepository.insert(1L, 100L, CHARGE, currentTime);
+        pointHistoryRepository.insert(1L, 200L, CHARGE, currentTime);
+        pointHistoryRepository.insert(1L, 300L, CHARGE, currentTime);
 
         // when
         List<PointHistory> resultPointHistories = pointHistoryRepository.selectAllByUserId(1L);
 
         // then
-        assertEquals(pointHistories, resultPointHistories);
+        assertAll(
+                () -> assertNotNull(resultPointHistories),
+                () -> assertEquals(3, resultPointHistories.size()),
+                () -> assertThat(resultPointHistories).containsExactlyInAnyOrder(
+                        new PointHistory(1L, 1L, 100L, CHARGE, currentTime),
+                        new PointHistory(2L, 1L, 200L, CHARGE, currentTime),
+                        new PointHistory(3L, 1L, 300L, CHARGE, currentTime)
+                )
+        );
     }
 
 }
