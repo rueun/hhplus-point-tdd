e5909a56d424771a0200372b088a3ef6a10b1c56
rueun <dkfah1229@naver.com>
2024-09-26 00:06:40 +0900

    refactor: 불필요한 정책 서비스 제거

--- src/main/java/io/hhplus/tdd/point/domain/service/UserPointPolicyService.java
+++
@@ -1,8 +0,0 @@
-package io.hhplus.tdd.point.domain.service;
-
-import io.hhplus.tdd.point.domain.model.entity.UserPoint;
-
-public interface UserPointPolicyService {
-    void validateCharge(UserPoint userPoint, long amount);
-    void validateUse(UserPoint userPoint, long amount);
-}
